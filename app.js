bc8518f2a784446c42285bc3db46b2c464225cc9
sharif23pak <160602479+sharif23pak@users.noreply.github.com>
2024-04-18 12:00:45 +0100

    learning

---
+++ app.js
@@ -0,0 +1,128 @@
+
+
+// let firstName = prompt("Enter your first name:");
+// let lastName = prompt("Enter your last name:");
+
+
+
+// let fullName = firstName + " " + lastName;
+
+
+// alert("Hello, " + fullName + "! Welcome!");
+
+
+// let favoriteMobile = prompt("What is your favorite mobile phone model?");
+
+
+// let inputLength = favoriteMobile.length;
+
+
+// document.write("<h2>Length of your favorite mobile phone model:</h2>");
+// document.write("Your favorite mobile phone model is: " + favoriteMobile + "<br>");
+// document.write("Length of your input is: " + inputLength);
+// The word
+// The word
+// let word = "Hello World";
+
+
+// let lastIndex = word.lastIndexOf("l");
+
+
+// document.write("<h2>Last Index of letter 'l' in the word 'Hello World':</h2>");
+// document.write("The word is: " + word + "<br>");
+// document.write("Last Index of letter 'l' is: " + lastIndex);
+
+// Taking user input
+// var userInput = prompt("Enter your text:");
+
+
+// var uppercaseText = userInput.toUpperCase();
+
+
+// document.write("<h2>Converted Text to Uppercase:</h2>");
+// document.write("Original Text: " + userInput + "<br>");
+// document.write("Uppercase Text: " + uppercaseText);
+
+// Function to convert input to title case
+
+
+
+// let userInput = prompt("Enter your text:");
+
+
+// let titleCaseText = toTitleCase(userInput);
+
+
+// document.write("<h2>Converted Text to Title Case:</h2>");
+// document.write("Original Text: " + userInput + "<br>");
+// document.write("Title Case Text: " + titleCaseText);
+// Variable num
+// var num = 35.36;
+
+
+// var strNum = num.toString().replace(".", "");
+
+
+// document.write("<h2>Converted Number to String and Removed Dot:</h2>");
+// document.write("Original Number: " + num + "<br>");
+// document.write("Converted String without Dot: " + strNum);
+
+
+
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+
+// function searchItem(item) {
+    
+//     var lowercaseItem = item.toLowerCase();
+
+    
+//     var found = false;
+//     for (var i = 0; i < A.length; i++) {
+//         if (A[i].toLowerCase() === lowercaseItem) {
+//             found = true;
+//             break;
+//         }
+//     }
+
+//     if (found) {
+//         alert("Yes, '" + item + "' is found in the list.");
+//     } else {
+//         alert("No, '" + item + "' is not found in the list.");
+//     }
+// }
+
+// var userInput = prompt("Enter item to search:");
+
+// searchItem(userInput);
+
+
+// var userInput = prompt("Enter a string:");
+
+
+// var lastCharacter = userInput.charAt(userInput.length - 1);
+
+
+// document.write("<h2>Last Character of the Input:</h2>");
+// document.write("Input: " + userInput + "<br>");
+// document.write("Last Character: " + lastCharacter);
+
+
+
+
+// var str = "The quick brown fox jumps over the lazy dog";
+
+// var lowercaseStr = str.toLowerCase();
+
+// var words = lowercaseStr.split(" ");
+
+// var count = 0;
+// for (var i = 0; i < words.length; i++) {
+//     if (words[i] === "the") {
+//         count++;
+//     }
+// }
+
+// document.write("<h2>Occurrences of the Word 'the':</h2>");
+// document.write("Given string: " + str + "<br>");
+// document.write("Number of occurrences of 'the': " + count);
